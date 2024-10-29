c55990107137b0d5fa04e7ac839d9cabf7a1e219
rebe-siro <rebekasilva1808@gmail.com>
2024-10-28 23:31:35 -0400

    Add files via upload

---
+++ src/main/java/org/biblioteca/model/Extension.java
@@ -0,0 +1,60 @@
+package org.biblioteca.model;
+
+import java.util.Date;
+
+public class Extension {
+    private int code;
+    private String user;
+    private Date star_Date;
+    private Date end_Date;
+    private Date new_End_Date;
+    private boolean status;
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public Date getStar_Date() {
+        return star_Date;
+    }
+
+    public void setStar_Date(Date star_Date) {
+        this.star_Date = star_Date;
+    }
+
+    public Date getEnd_Date() {
+        return end_Date;
+    }
+
+    public void setEnd_Date(Date end_Date) {
+        this.end_Date = end_Date;
+    }
+
+    public Date getNew_End_Date() {
+        return new_End_Date;
+    }
+
+    public void setNew_End_Date(Date new_End_Date) {
+        this.new_End_Date = new_End_Date;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+}
