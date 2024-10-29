0f144fbc31e692ae42c01c10de24dce4973d0d8a
MarySPerez <108902295+MarySPerez@users.noreply.github.com>
2024-10-29 17:29:55 -0400

    Update LoanResources.java

--- src/main/java/org/biblioteca/model/LoanResources.java
+++ src/main/java/org/biblioteca/model/LoanResources.java
@@ -1,1 +1,38 @@
+package org.biblioteca.model;
+
+public class LoanResources{
+        private int code;
+        private String description;
+        private boolean status;
+
+public LoanResources(int code, String description, Boolean status) {
+	this.code = code;
+	this.description = description;
+	this.status = status;
+}
+        
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+}
 
