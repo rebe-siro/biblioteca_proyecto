ea505af9235d48bb75df8085a6cd122544de77ee
MarySPerez <108902295+MarySPerez@users.noreply.github.com>
2024-10-29 11:38:03 -0400

    Update Carrera.java

--- src/main/java/org/biblioteca/model/Carrera.java
+++ src/main/java/org/biblioteca/model/Carrera.java
@@ -2,30 +2,63 @@ package org.biblioteca.model;
 
 public class Carrera {
     private int code;
+    private String name;
     private String description;
-    private boolean status;
+    private String decanato;
+	private boolean status;
+    
+public Carrera() {}
 
-    public int getCode() {
-        return code;
-    }
+public Carrera(int code, String name, String description, String decanato, boolean status) {
+	this.code = code;
+	this.name = name;
+	this.description = description;
+	this.decanato = decanato;
+	this.status = status;
+}
 
-    public void setCode(int code) {
-        this.code = code;
-    }
 
-    public String getDescription() {
-        return description;
-    }
+public int getCode() {
+    return code;
+	}
 
-    public void setDescription(String description) {
-        this.description = description;
-    }
+public void setCode(int code) {
+    this.code = code;
+	}
 
-    public boolean isStatus() {
-        return status;
-    }
 
-    public void setStatus(boolean status) {
-        this.status = status;
-    }
+public String getName() {
+    return name;
+	}
+
+public void setName(String name) {
+    this.name = name;
+	}
+
+
+public String getDescription() {
+    return description;
+	}
+
+public void setDescription(String description) {
+    this.description = description;
+	}
+
+
+public String getDecanato() {
+    return decanato;
+	}
+
+public void setDecanto(String decanato) {
+    this.decanato = decanato;
+	}
+
+
+public boolean getStatus() {
+    return status;
+	}
+
+public void setStatus(boolean status) {
+    this.status = status;
+	}
 }
