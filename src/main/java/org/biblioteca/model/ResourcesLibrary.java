f100e563adcdb6fa2b2a523d321ff32adc8773d7
MarySPerez <108902295+MarySPerez@users.noreply.github.com>
2024-10-29 17:30:55 -0400

    Update ResourcesLibrary.java

--- src/main/java/org/biblioteca/model/ResourcesLibrary.java
+++ src/main/java/org/biblioteca/model/ResourcesLibrary.java
@@ -1,1 +1,37 @@
+package org.biblioteca.model;
 
+public class ResourcesLibrary {
+    private int code;
+    private String description;
+    private boolean status;
+
+public ResourcesLibrary(int code, String description, Boolean status) {
+	this.code = code;
+	this.description = description;
+	this.status = status;
+}
+    
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(Boolean status) {
+        this.status = status;
+    }
+}
