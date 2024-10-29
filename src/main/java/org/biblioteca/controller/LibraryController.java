343ba04d16df1c204b02f713d6f680fde3e6fa3e
MarySPerez <108902295+MarySPerez@users.noreply.github.com>
2024-10-29 19:15:04 -0400

    Update LibraryController.java

--- src/main/java/org/biblioteca/controller/LibraryController.java
+++ src/main/java/org/biblioteca/controller/LibraryController.java
@@ -1,1 +1,9 @@
 package org.biblioteca.controller;
+
+public class Library { 
+  private Usuario modelo; 
+  private UsuarioView vista; 
+  
+public UsuarioController(Usuario modelo, UsuarioView vista) {
+  this.modelo = modelo; 
+  this.vista = vista;
