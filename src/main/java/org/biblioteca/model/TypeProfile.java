17157445145c7dd470479c7449bdad8217663b4c
MarySPerez <108902295+MarySPerez@users.noreply.github.com>
2024-10-29 17:14:43 -0400

    Update TypeProfile.java

--- src/main/java/org/biblioteca/model/TypeProfile.java
+++ src/main/java/org/biblioteca/model/TypeProfile.java
@@ -1,1 +1,38 @@
+package org.biblioteca.model;
+
+public class TypeProfile {
+    private int code;
+    private String name;
+    private boolean status;
+
+public TypeProfile (int code, String name, Boolean status) {
+	this.code = code;
+	this.name = name;
+	this.status = status;
+}
+    
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(Boolean status) {
+        this.status = status;
+    }
+}
 
