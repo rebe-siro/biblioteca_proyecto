b8a57c168e9a33d6e7135be0a875248c0d297224
MarySPerez <108902295+MarySPerez@users.noreply.github.com>
2024-10-29 17:04:34 -0400

    Update and rename Tipo_Usuario.java to TypeUsers.java

--- src/main/java/org/biblioteca/model/TypeUsers.java
+++ src/main/java/org/biblioteca/model/TypeUsers.java
@@ -1,6 +1,6 @@
 package org.biblioteca.model;
 
-public class Tipo_Usuario {
+public class TypeUsers {
         private int code;
         private String description;
         private boolean status;
